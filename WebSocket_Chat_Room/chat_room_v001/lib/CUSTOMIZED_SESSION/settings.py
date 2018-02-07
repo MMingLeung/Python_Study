bd742ce2a79ae74c51d96f97ff60a92cc2ce354b
MMingLeung <mingmingleung1991@gmail.com>
2018-02-07 17:50:30 +0800

    del

--- WebSocket_Chat_Room/chat_room_v001/lib/CUSTOMIZED_SESSION/settings.py
+++
@@ -1,3 +0,0 @@
-SESSION_ENGINE = 'lib.CUSTOMIZED_SESSION.my_session.CachedSession'
-EXPIRE_TIME = 300
-SESSION_ID = '__session_id__'
--- WebSocket_Chat_Room/chat_room_v001/static/settings.py
+++
@@ -1,9 +0,0 @@
-DB_USER_NAME = 'root'
-
-DB_PWD = 'qwert'
-
-DB_NAME = 'chat_room'
-
-DB_HOST = '127.0.0.1'
-
-CHARSET = 'utf8'
