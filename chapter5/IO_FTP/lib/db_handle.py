84078dc4a0f4ba53c6c8a921f3676a6baa590704
MMingLeung <mingmingleung1991@gmail.com>
2018-02-25 16:31:04 +0800

    remove

--- chapter5/IO_FTP/lib/db_handle.py
+++
@@ -1,9 +0,0 @@
-from conf import settings
-import json
-
-def load_db():
-    '读取数据库'
-    with open(settings.db_BASE_DIR, 'r') as file:
-        data = json.loads(file.read())
-        return data
-
--- chapter5/IO_FTP/lib/roll_bar.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-def roll_bar(recv_size, filesize):
-    '进度条'
-    size = float(recv_size) / float(filesize) * 100
-    p = "\r%d%% :%s" % (size, int(size) * '*')
-    sys.stdout.write(p)
-    sys.stdout.flush()
